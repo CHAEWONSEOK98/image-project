73dfb50d1c9013860fcc8ff7c93975b1de30e253
CHAEWONSEOK98 <7dugod7@naver.com>
2023-08-02 23:58:25 +0900

    front - select image file

--- front/src/components/UploadForm.tsx
+++ front/src/components/UploadForm.tsx
@@ -1,8 +1,18 @@
+import { useState } from 'react';
+
 const UploadForm = () => {
+  const [file, setFile] = useState(null);
+  const [fileName, setFileName] = useState('');
+
+  const handleSelectImage = (event) => {
+    const imageFile = event.target.files[0];
+    setFile(imageFile);
+    setFileName(imageFile.name);
+  };
   return (
     <form className="absolute left-1/2 top-1/2 -translate-x-[50%] -translate-y-[50%]">
-      <label htmlFor="image">사진</label>
-      <input id="image" type="file" />
+      <label htmlFor="image">{fileName}</label>
+      <input id="image" type="file" onChange={handleSelectImage} />
       <button type="submit">제출</button>
     </form>
   );

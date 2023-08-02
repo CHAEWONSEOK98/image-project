83300fd802aa2c33f61561c7b888c33d19ac16c6
CHAEWONSEOK98 <7dugod7@naver.com>
2023-08-03 02:13:57 +0900

    front - axios.post formData

--- front/src/components/UploadForm.tsx
+++ front/src/components/UploadForm.tsx
@@ -1,18 +1,40 @@
+import axios from 'axios';
 import { useState } from 'react';
 
 const UploadForm = () => {
-  const [file, setFile] = useState(null);
+  const [file, setFile] = useState<File[]>([]);
   const [fileName, setFileName] = useState('');
 
-  const handleSelectImage = (event) => {
-    const imageFile = event.target.files[0];
-    setFile(imageFile);
-    setFileName(imageFile.name);
+  const handleSubmit = async (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    const formData = new FormData();
+    formData.append('image', file[0]);
+
+    try {
+      const res = await axios.post('api/upload', formData, {
+        headers: { 'Content-Type': 'multipart/form-data' },
+      });
+      console.log({ res });
+      alert('success');
+    } catch (error) {
+      console.log(error);
+      alert('fail');
+    }
   };
+
+  const handleSelectFile = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const imageFile = Array.from(event.target.files || []);
+    setFile(imageFile);
+    setFileName(imageFile[0].name);
+  };
+
   return (
-    <form className="absolute left-1/2 top-1/2 -translate-x-[50%] -translate-y-[50%]">
+    <form
+      className="absolute left-1/2 top-1/2 -translate-x-[50%] -translate-y-[50%]"
+      onSubmit={handleSubmit}
+    >
       <label htmlFor="image">{fileName}</label>
-      <input id="image" type="file" onChange={handleSelectImage} />
+      <input id="image" type="file" onChange={handleSelectFile} />
       <button type="submit">제출</button>
     </form>
   );

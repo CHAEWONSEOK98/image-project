d4bd9cf764957ef02c858304a8634871c30aae12
CHAEWONSEOK98 <7dugod7@naver.com>
2023-08-05 01:54:31 +0900

    front - preview image

--- front/src/components/UploadForm.tsx
+++ front/src/components/UploadForm.tsx
@@ -3,7 +3,18 @@ import { useState } from 'react';
 
 const UploadForm = () => {
   const [file, setFile] = useState<File[]>([]);
-  const [fileName, setFileName] = useState('Drag files to upload');
+  const [fileName, setFileName] = useState<string>('Drag files to upload');
+  const [imgSrc, setImgSrc] = useState<any>(null);
+
+  const handleSelectFile = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const imageFile = Array.from(event.target.files || []);
+    setFile(imageFile);
+    setFileName(imageFile[0].name);
+
+    const fileReader = new FileReader();
+    fileReader.readAsDataURL(imageFile[0]);
+    fileReader.onload = () => setImgSrc(fileReader.result);
+  };
 
   const handleSubmit = async (event: React.FormEvent<HTMLFormElement>) => {
     event.preventDefault();
@@ -14,42 +25,47 @@ const UploadForm = () => {
       const res = await axios.post('api/upload', formData, {
         headers: { 'Content-Type': 'multipart/form-data' },
       });
-      console.log({ res });
       alert('success');
+      setFileName('Drag files to upload');
+      setImgSrc(null);
     } catch (error) {
       console.log(error);
       alert('fail');
+      setFileName('Drag files to upload');
+      setImgSrc(null);
     }
   };
 
-  const handleSelectFile = (event: React.ChangeEvent<HTMLInputElement>) => {
-    const imageFile = Array.from(event.target.files || []);
-    setFile(imageFile);
-    setFileName(imageFile[0].name);
-  };
-
   return (
-    <form
-      className="absolute left-1/2 top-1/2 -translate-x-[50%] -translate-y-[50%] border-[2px] border-solid p-20 shadow-md"
-      onSubmit={handleSubmit}
-    >
-      <div className="relative flex h-[15rem] w-[20rem] items-center justify-center  rounded-md border-[2px] border-dashed border-black  ">
-        <span className="font-bold">{fileName}</span>
-        <input
-          className="absolute h-full w-full cursor-pointer opacity-0 "
-          id="image"
-          type="file"
-          onChange={handleSelectFile}
-        />
-      </div>
+    <main className="flex h-screen w-screen flex-col items-center justify-center space-y-10 md:flex md:flex-row-reverse md:justify-around">
+      <section>
+        <img className="rounded-md" src={imgSrc} alt={imgSrc} />
+      </section>
 
-      <button
-        type="submit"
-        className="mt-6 w-full rounded-md bg-[#0144FF] p-2 py-3 text-white"
-      >
-        Upload
-      </button>
-    </form>
+      <section>
+        <form
+          className="rounded-md border-[3px] border-solid border-blue-500 p-16 "
+          onSubmit={handleSubmit}
+        >
+          <div className="flex h-64 w-80 items-center justify-center rounded-md border-[2px]  border-dashed border-black">
+            <span className="absolute font-bold">{fileName}</span>
+            <input
+              className="h-full w-full cursor-pointer opacity-0"
+              id="image"
+              type="file"
+              onChange={handleSelectFile}
+            />
+          </div>
+
+          <button
+            type="submit"
+            className="mt-6 w-full rounded-md bg-[#0144FF] p-2 py-3 text-white"
+          >
+            Upload
+          </button>
+        </form>
+      </section>
+    </main>
   );
 };
 

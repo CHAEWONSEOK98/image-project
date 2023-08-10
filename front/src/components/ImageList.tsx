f05e2f5474c90633807a0fb26abcaff742023625
CHAEWONSEOK98 <7dugod7@naver.com>
2023-08-10 16:56:19 +0900

    front - cursor-based pagination

--- front/src/components/ImageList.tsx
+++ front/src/components/ImageList.tsx
@@ -1,36 +1,45 @@
 import axios from 'axios';
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import { useRecoilState } from 'recoil';
 import { imageState } from '../atoms/atoms';
 
 const ImageList = () => {
   const [images, setImages] = useRecoilState(imageState);
+  const [imageUrl, setImageUrl] = useState('/api/images');
 
   const getImageData = async (): Promise<void> => {
     try {
-      const response = await axios.get('/api/images');
-      setImages(response.data);
+      const response = await axios.get(imageUrl);
+      setImages((prev) => [...prev, ...response.data]);
     } catch (error) {
       console.log(error);
       throw new Error('Failed to get Image');
     }
   };
-  console.log(images);
+
+  const loadMoreImageData = () => {
+    if (images.length === 0) return;
+    const lastImageId = images[images.length - 1]._id;
+    setImageUrl(`/api/images?lastId=${lastImageId}`);
+  };
+
   useEffect(() => {
     getImageData();
-  }, []);
-
-  const imageList = images.map((image) => (
-    <img
-      className="rounded-lg"
-      key={image.key}
-      src={`http://localhost:5000/public/${image.key}`}
-    />
-  ));
+  }, [imageUrl]);
 
   return (
-    <div className="grid grid-cols-2 grid-rows-2 gap-4 p-8">
-      {images.length > 0 && imageList}
+    <div className="flex flex-col">
+      <div className="grid grid-cols-2 grid-rows-2 gap-4 p-8">
+        {images.length > 0 &&
+          images.map((image) => (
+            <img
+              className="rounded-lg"
+              key={image._id}
+              src={`http://localhost:5000/public/${image.key}`}
+            />
+          ))}
+      </div>
+      <button onClick={loadMoreImageData}>Load More Images</button>
     </div>
   );
 };
